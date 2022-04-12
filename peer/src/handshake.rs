29ade7b585b1a49adf9445fe7395a9e6cb9ab133
startdusk <522383345@qq.com>
2022-04-12 22:58:27 +0800

    feat✨:  add handshake

--- peer/src/handshake.rs
+++ peer/src/handshake.rs
@@ -1,7 +1,10 @@
 use std::io::BufReader;
 use std::io::BufWriter;
+use std::io::Cursor;
 use std::io::Read;
 use std::io::Write;
+
+use bytes::{Buf, BufMut, BytesMut};
 
 use crate::error::PeerError;
 
@@ -13,11 +16,13 @@ pub struct Handshake {
     pub peer_id: [u8; 20],
 }
 
+pub const PROTOCOL_STRING: &'static str = "BitTorrent protocol";
+
 impl Handshake {
     pub fn new(info_hash: [u8; 20], peer_id: [u8; 20]) -> Self {
         Self {
             pstrlen: [0; 19],
-            pstr: "BitTorrent protocol",
+            pstr: PROTOCOL_STRING,
             reserved: [0; 8],
             info_hash,
             peer_id,
@@ -29,24 +34,34 @@ impl Handshake {
     }
 
     // <pstrlen><pstr><reserved><info_hash><peer_id>
-    pub fn encode(&self) -> Result<Vec<u8>, PeerError> {
-        let mut buf = Vec::with_capacity(self.pstr.len() + 49);
-        buf.write_all(&self.pstrlen)?;
-        buf.write_all(&self.pstr.as_bytes())?;
-        buf.write_all(&self.reserved)?;
-        buf.write_all(&self.info_hash)?;
-        buf.write_all(&self.peer_id)?;
-        Ok(buf)
+    pub fn encode(&self) -> Vec<u8> {
+        let mut buf = BytesMut::new();
+        buf.put_u8(self.pstr.len() as u8);
+        buf.extend_from_slice(&self.pstr.as_bytes());
+        buf.extend_from_slice(&self.reserved);
+        buf.extend_from_slice(&self.info_hash);
+        buf.extend_from_slice(&self.peer_id);
+        buf.to_vec()
     }
 
-    pub fn from_bytes<T>(bytes: T) -> Result<Handshake, PeerError>
+    pub fn from_bytes<T>(buf: T) -> Result<Handshake, PeerError>
     where
         T: AsRef<[u8]>,
     {
-        let mut buf = BufReader::new(bytes.as_ref());
-        let mut len_buf: Vec<u8> = Vec::with_capacity(1);
-        buf.read(&mut len_buf)?;
-
+        let mut tmp_buf = Cursor::new(&buf);
+        let pstrlen = tmp_buf.get_u8() as u8;
+        // protocol string
+        // let mut prot = [0; 19];
+        // buf.copy_to_slice(&mut prot);
+        // // reserved field
+        // let mut reserved = [0; 8];
+        // buf.copy_to_slice(&mut reserved);
+        // // info hash
+        // let mut info_hash = [0; 20];
+        // buf.copy_to_slice(&mut info_hash);
+        // // peer id
+        // let mut peer_id = [0; 20];
+        // buf.copy_to_slice(&mut peer_id);
         todo!()
     }
 }
